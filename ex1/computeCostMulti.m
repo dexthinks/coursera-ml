0804d052188f941146ccc91f333ab25a6d7f5cd4
Dexter Darwich <dex.darwich@gmail.com>
2021-06-14 21:53:25 -0600

    Week 2 * Excercise 1 * Linear regression with multiple variables

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -13,9 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+xtheta = X*theta;
+J = (1/(2*m)) * (((xtheta - y)') * (xtheta - y));
 
 % =========================================================================
 
