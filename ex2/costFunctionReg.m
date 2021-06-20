b577dd9ebdf7afcc8ae33606a2442ae5a734497e
Dexter Darwich <dex.darwich@gmail.com>
2021-06-19 22:01:57 -0600

    Week 3 * Excercise 2 - Regularized logistic regression

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,9 +17,29 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+# JTheta = (1/m Sum m = 1 to m, (-yi*log(hTheta(xi)) - (1-yi)*log(1-hTheta(xi))))
+#          + (lambda / (2*m)) Sum j = 1 to n, thetaj^2
+# Grad = 1/m Sum m = 1 to m, (hTheta - yi) * xj                      For j = 0
+# Grad = 1/m Sum m = 1 to m, (hTheta - yi) * xj + (lambda/m) *thetaj For j >= 1
 
+hTheta = sigmoid(theta' * X');
+n = length(theta);
+sumTheta = 0;
+for j = 2:n
+  sumTheta += theta(j)^2;
+end
 
+for i = 1:m
+  J += ((-y(i) * log(hTheta(i)) - (1 - y(i)) * log(1 - hTheta(i)))) / m;
+       
+  grad += (((hTheta(i) - y(i)).*X(i,:))') / m;  
+end
 
+J += (lambda / (2 * m)) * sumTheta;
+
+for k = 2:n
+  grad(k) += (lambda / m) * theta(k);
+endfor
 
 
 % =============================================================
